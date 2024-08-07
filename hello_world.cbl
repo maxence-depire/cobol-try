e0a7c4d83ab5bc3f24d7fb3b6983ecf6e335c139
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-08-08 00:27:19 +0200

    My first program in Cobol

---
+++ hello_world.cbl
@@ -0,0 +1,9 @@
+    *>    Compile with the command : cobc -x <scirpt.name.cbl>
+       
+       identification division.
+       program-id. hello_world.
+       
+       
+       procedure division.
+           display 'Hello World !'.                                     
+       stop run.
