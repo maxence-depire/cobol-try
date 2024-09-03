72d0a23341ceb79dcc10e0c4cfb6ebbd13151bbc
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-09-03 23:44:42 +0200

    update conditions.

--- conditions.cbl
+++ conditions.cbl
@@ -8,14 +8,24 @@
            88 passable value 10 thru 11.99.
            88 assez_bien value 12 thru 13.99.
            88 bien value 14 thru 19.99.                                    
-           88 parfait value 10.
+           88 parfait value 20.
+
+           77 coef pic 99.
 
        procedure division.
        
-           move 20.00 to note.
+           initialize note.
+           initialize coef.
 
-           if bien then
-               display 'ici'
-           end-if.                                                      
+           move 20 to note.
+           move 04 to coef.
+
+           if bien or parfait then                                   
+               display 'C est une bonne note ça'
+           end-if.         
+
+           if coef > 2 then
+               display "ah ouais gros coef"
+           end-if.                                                       
 
        stop run.
