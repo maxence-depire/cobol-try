266478d32c90557b8da3f336ef79ba08a4dfe4ca
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-09-04 21:53:28 +0200

    add loop with increment.

---
+++ loop_for_increment.cbl
@@ -0,0 +1,16 @@
+       identification division.
+       program-id. loop_for_increment.                                            
+       
+       data division.
+       working-storage section.
+           77 a PIC 99.
+
+       procedure division.
+
+           initialize a.
+
+           perform varying a from 1 by 1 until a > 10
+               display a
+           end-perform.
+
+       stop run.
