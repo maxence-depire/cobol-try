cbd8898528213db614d569d605f3bb36ee244019
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-09-06 08:57:29 +0200

    adding table file.

---
+++ tables.cbl
@@ -0,0 +1,10 @@
+       identification division.
+       program-id. tables.
+       
+       data division.
+       working-storage section.
+           
+           01 tableau.
+               02 entier pic 9 occurs 10.                                                    
+
+       stop run.
